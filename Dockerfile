dfe391bf3361db2d1ee04c88eab02dae923cc8e3
Angatar <philippe.lieutaud@univ-amu.fr>
2020-04-24 12:22:58 +0000

    adding dockerfiles and config files

---
+++ Dockerfile
@@ -0,0 +1,48 @@
+FROM debian:buster-slim
+MAINTAINER d3fk 
+
+ENV DEBIAN_FRONTEND noninteractive
+ENV DEBCONF_NONINTERACTIVE_SEEN true
+
+ENV URL_HOST lists.example.com
+ENV EMAIL_HOST lists.example.com
+ENV MASTER_PASSWORD example
+ENV LIST_ADMIN admin@lists.example.com
+ENV LIST_LANGUAGE_CODE en
+# if you are not behind a loadbalancer or if you can get the origin IP it might be useful to enable SPF to avoid identity usurpation
+ENV ENABLE_SPF FALSE
+# Add a root dir to mailman so that it is not obvious for bots, don't forget the trailling slash or let empty if root has to be "/"
+ENV URL_ROOT lists/
+# Possible values for URL_PATTERN are http and https 
+#... might be usefull with an ingress and letsencrypt certmanager for example
+ENV URL_PATTERN http 
+
+COPY conf/run.sh /
+
+RUN apt-get update \
+    && apt-get -y upgrade \
+    && apt-get install -y mailman exim4 apache2 \
+    && chmod +x /run.sh
+
+COPY conf/00_local_macros /etc/exim4/conf.d/main/
+COPY conf/04_mailman_options /etc/exim4/conf.d/main/
+COPY conf/450_mailman_aliases /etc/exim4/conf.d/router/
+COPY conf/40_mailman_pipe /etc/exim4/conf.d/transport/
+COPY conf/update-exim4.conf.conf /etc/exim4/update-exim4.conf.conf
+
+COPY conf/mm_cfg.py /etc/mailman/mm_cfg.py
+
+COPY conf/mailman.conf /etc/apache2/sites-available/
+
+COPY conf/aliases /etc/aliases
+
+VOLUME /var/log/mailman
+VOLUME /var/log/exim4
+VOLUME /var/log/apache2
+VOLUME /var/lib/mailman/archives
+VOLUME /var/lib/mailman/lists
+VOLUME /etc/exim4/tls.d
+
+EXPOSE 25 80
+
+CMD ["/run.sh"]
---
+++ conf/run.sh
@@ -0,0 +1,165 @@
+#!/bin/bash
+# By d3fk
+
+#set default email originator /  and root user aliase
+echo "root: ${LIST_ADMIN}" > /etc/email-addresses
+/bin/sed -i "s/admin@example\.com/${LIST_ADMIN}/" /etc/aliases
+
+
+#declare Hostname in hostname and mailname files
+echo "${EMAIL_HOST}" > /etc/hostname 
+echo ${EMAIL_HOST} > /etc/mailname
+
+hostname -I |  awk -v hostname=${EMAIL_HOST} '{disp=$1"    " hostname; print disp}' >> /etc/hosts
+
+#Change owner:group of mailman directory
+chown -R list:list /var/lib/mailman/
+
+#Create docroot 
+mkdir /var/www/lists
+echo "<html><h2>Welcome to ${URL_HOST}</h2></html>" > /var/www/lists/index.html
+
+mailmancfg='/etc/mailman/mm_cfg.py'
+
+# define the URL pattern for mailman
+if [ $URL_PATTERN != "http" ]; then
+    echo "DEFAULT_URL_PATTERN = 'https://%s/${URL_ROOT}'" >> $mailmancfg
+else
+    echo "DEFAULT_URL_PATTERN = 'http://%s/'" >> $mailmancfg
+fi
+
+# enable spf check if requested
+if [ $ENABLE_SPF ]; then
+    echo "installing SPF tools before enabling"
+    apt-get install -y spf-tools-perl 
+
+    /bin/sed -i "s/#CHECK_RCPT_SPF/CHECK_RCPT_SPF/" /etc/exim4/conf.d/main/00_local_macros
+    echo "SPF CHECK is now enabled"
+fi
+
+# Replace default hostnames with runtime values:
+/bin/sed -i "s/lists\.example\.com/${EMAIL_HOST}/" /etc/exim4/conf.d/main/00_local_macros
+/bin/sed -i "s/lists\.example\.com/${EMAIL_HOST}/" /etc/exim4/conf.d/main/04_mailman_options
+/bin/sed -i "s/lists\.example\.com/${EMAIL_HOST}/" /etc/exim4/update-exim4.conf.conf
+/bin/sed -i "s/lists\.example\.com/${URL_HOST}/" /etc/apache2/sites-available/mailman.conf
+/bin/sed -i "s/URL_ROOT\//${URL_ROOT}/" /etc/apache2/sites-available/mailman.conf
+
+#/bin/sed -i "s/DEFAULT_EMAIL_HOST.*\=.*/DEFAULT_EMAIL_HOST\ \=\ \'${EMAIL_HOST}\'/" $mailmancfg
+/bin/sed -i "s/lists\.example\.com/${EMAIL_HOST}/" $mailmancfg
+/bin/sed -i "s/DEFAULT_URL_HOST.*\=.*/DEFAULT_URL_HOST\ \=\ \'${URL_HOST}\'/" $mailmancfg
+/bin/sed -i "s/DEFAULT_SERVER_LANGUAGE.*\=.*/DEFAULT_SERVER_LANGUAGE\ \=\ \'${LIST_LANGUAGE_CODE}\'/" $mailmancfg
+
+
+echo -n "Setting up Mailman..."
+{
+#        debconf-set-selections /mailman-config.cfg
+        dpkg-reconfigure mailman
+
+#      especialy for debian:buster
+       mkdir /var/run/mailman
+       chown list:list /var/run/mailman/
+       ln -s /var/lib/mailman/bin/mailmanctl /etc/init.d/mailman
+
+}
+
+
+
+echo -n "Initializing mailing lists..."
+{
+	/usr/sbin/mmsitepass ${MASTER_PASSWORD}
+	/usr/sbin/newlist -q -l ${LIST_LANGUAGE_CODE} mailman ${LIST_ADMIN} ${MASTER_PASSWORD}
+}
+
+# Addaliases and update them:
+#cat << EOA >> /etc/aliases
+#mailman:              "|/var/lib/mailman/mail/mailman post mailman"
+#mailman-admin:        "|/var/lib/mailman/mail/mailman admin mailman"
+#mailman-bounces:      "|/var/lib/mailman/mail/mailman bounces mailman"
+#mailman-confirm:      "|/var/lib/mailman/mail/mailman confirm mailman"
+#mailman-join:         "|/var/lib/mailman/mail/mailman join mailman"
+#mailman-leave:        "|/var/lib/mailman/mail/mailman leave mailman"
+#mailman-owner:        "|/var/lib/mailman/mail/mailman owner mailman"
+#mailman-request:      "|/var/lib/mailman/mail/mailman request mailman"
+#mailman-subscribe:    "|/var/lib/mailman/mail/mailman subscribe mailman"
+#mailman-unsubscribe:  "|/var/lib/mailman/mail/mailman unsubscribe mailman"
+#EOA
+#/usr/bin/newaliases
+
+#update aliases
+/usr/bin/newaliases
+
+
+
+echo -n "Setting up Apache web server..."
+{
+	a2enmod cgi
+        a2dissite 000-default
+	a2ensite mailman.conf
+# edit apache default security.conf for production        
+        /bin/sed -i "s/ServerSignature On/ServerSignature Off/" /etc/apache2/conf-available/security.conf
+        /bin/sed -i "s/ServerTokens OS/ServerTokens Prod/" /etc/apache2/conf-available/security.conf
+}
+
+echo -n "Setting up RSA keys for DKIM..."
+{
+	if [ ! -f /etc/exim4/tls.d/private.pem ]; then
+		mkdir -p /etc/exim4/tls.d
+		openssl genrsa -out /etc/exim4/tls.d/private.pem 2048
+		openssl rsa -in /etc/exim4/tls.d/private.pem -out /etc/exim4/tls.d/public.pem -pubout
+	fi
+}
+
+key=$(sed -e '/^-/d' /etc/exim4/tls.d/public.pem|paste -sd '' -)
+
+echo -n "setting up cert for TLS"
+{
+        if [ ! -f /etc/exim4/exim.key ]; then
+                openssl req -x509 -sha256 -days 9000 -nodes -newkey rsa:4096 -keyout /etc/exim4/exim.key -out /etc/exim4/exim.crt -subj "/O=${EMAIL_HOST}/OU=IT Department/CN=${EMAIL_HOST}"
+
+        fi
+}
+
+echo -n "Fixing exim4 permissions ..."
+{
+        chown -R Debian-exim:Debian-exim /etc/exim4
+}
+
+#build updated exim config file
+echo -n "Setting up Exim4..."
+{
+
+        update-exim4.conf
+
+}
+
+
+
+echo -n "Fixing mailman permissons ..."
+{
+	/usr/lib/mailman/bin/check_perms -f > /dev/null
+}
+
+echo -n "Starting up services..."
+{
+	/etc/init.d/exim4 start
+	/etc/init.d/mailman start
+}
+
+cat << EOB
+
+    ***********************************************
+    *                                             *
+    *   TO COMPLETE DKIM SETUP, COPY THE          *
+    *   FOLLOWING CODE INTO A NEW TXT RECORD      *
+    *   IN YOUR DNS SERVER:                       *
+    *                                             *
+    ***********************************************
+
+EOB
+echo "listsdkim._domainkey.${EMAIL_HOST} IN TXT \"k=rsa; p=$key\""
+echo
+echo
+echo '------------- CONTAINER UP AND RUNNING! -------------'
+
+# Starting apache2 in foreground 
+apachectl -DFOREGROUND -k start
